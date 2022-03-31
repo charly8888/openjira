d81d60da49e36750810a3139e3166f1508e22ec8
German Hornus <hornusgerman@gmail.com>
2022-03-31 15:38:23 -0600

    finished-section-of-sidebar

--- components/ui/Sidebar.tsx
+++ components/ui/Sidebar.tsx
@@ -1,13 +1,18 @@
 import { Box, Divider, Drawer, List, ListItem, ListItemIcon, ListItemText, Typography } from "@mui/material"
 import InboxOutlinedIcon from '@mui/icons-material/InboxOutlined';
 import MailOutlineOutlinedIcon from '@mui/icons-material/MailOutlineOutlined';
+import { useContext } from "react";
+import { UIContext } from "../../context/ui";
 const menuItems: string[] = ['Inbox', 'Starred', 'Send Email', 'Drafts']
 export const Sidebar = () => {
+
+  const { sidemenuOpen, closeSideMenu }  = useContext(UIContext)
+
   return (
     <Drawer
       anchor="left"
-      open={true}
-      onClose={() => console.log('cerrando') }
+      open={sidemenuOpen}
+      onClose={closeSideMenu}
     >
       <Box sx={{width: 250}}>
 
